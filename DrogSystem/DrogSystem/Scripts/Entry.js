72596c87e767238b215646fd0458c2aaa1881d9b
oassoinc1 <oscar.rincon@w2dit.com>
2020-02-05 16:30:29 -0500

    05/02/2020 IDU

--- DrogSystem/DrogSystem/Scripts/Entry.js
+++ DrogSystem/DrogSystem/Scripts/Entry.js
@@ -5,7 +5,7 @@
 //Load Data function  
 function loadData() {
     $.ajax({
-        url: "/Providers/List",
+        url: "/Entries/List",
         type: "GET",
         contentType: "application/json;charset=utf-8",
         dataType: "json",
@@ -35,13 +35,27 @@ function loadData() {
     });
 }
 
-function getbyID(TerceroId) {
+function AddFila() {
+    var html = '';
+    html = $('.tbody').html();
+    html += '<tr>';
+    html += '<td>' + $('$NombreProducto').val() + '</td>';
+    html += '<td>' + $('$NombreFabricante').val() + '</td>';
+    html += '<td>' + $('$Cantidad').val() + '</td>';
+    html += '<td>' + $('$Lote').val() + '</td>';
+    html += '<td>' + $('$FechaVence').val() + '</td>';
+    html += '<td><center><a href="#" onclick="return getbyID(1)">Editar</a>    |    <a href="#" onclick="Delete(1)">Eliminar</a></center></td>';
+    html += '</tr>';
+    $('.tbody').html(html);
+}
+
+function getbyID(EntryDetailId) {
     document.getElementById('myModalLabel').innerHTML = "Editar Tercero";
     $('#NombreTercero').css('border-color', 'lightgrey');
     $('#Codtercero').css('border-color', 'lightgrey');
     $('#ProviderTypeId').css('border-color', 'lightgrey');
     $.ajax({
-        url: "/Providers/getbyID/" + TerceroId,
+        url: "/Entries/getbyID/" + EntryDetailId,
         typr: "GET",
         contentType: "application/json;charset=UTF-8",
         dataType: "json",
@@ -69,7 +83,7 @@ function getbyID(TerceroId) {
     return false;
 }
 
-function Delete(TerceroId) {
+function Delete(EntryDetailId) {
     Swal.fire({
         title: "Estimado Usuario",
         text: "Esta seguro(a) que desea eliminar registro?",
@@ -82,7 +96,7 @@ function Delete(TerceroId) {
     }).then((result) => {
         if (result.value == true) {
             $.ajax({
-                url: "/Providers/Borrar/" + TerceroId,
+                url: "/Entries/Borrar/" + EntryDetailId,
                 type: "POST",
                 contentType: "application/json;charset=utf-8",
                 dataType: "json",
@@ -144,7 +158,7 @@ function Update() {
         TipoTercero: tipotercero,
     };
     $.ajax({
-        url: "/Providers/Editar",
+        url: "/Entries/Editar",
         data: JSON.stringify(userObj),
         type: "POST",
         contentType: "application/json;charset=utf-8",
@@ -177,19 +191,19 @@ function Update() {
 
 function validate() {
     var isValid = true;
-    if ($('#NombreTercero').val().trim() == "") {
-        $('#NombreTercero').css('border-color', 'Red');
+    if ($('#CodBarras').val().trim() == "") {
+        $('#CodBarras').css('border-color', 'Red');
         isValid = false;
     }
     else {
-        $('#NombreTercero').css('border-color', 'lightgrey');
+        $('#CodBarras').css('border-color', 'lightgrey');
     }
-    if ($('#Codtercero').val().trim() == "") {
-        $('#Codtercero').css('border-color', 'Red');
+    if ($('#Cantidad').val().trim() == "") {
+        $('#Cantidad').css('border-color', 'Red');
         isValid = false;
     }
     else {
-        $('#Codtercero').css('border-color', 'lightgrey');
+        $('#Cantidad').css('border-color', 'lightgrey');
     }
     return isValid;
 }
@@ -213,7 +227,7 @@ function Add() {
         TipoTercero: tipotercero,
     };
     $.ajax({
-        url: "/Providers/Crear",
+        url: "/Entries/Crear",
         data: JSON.stringify(userObj),
         type: "POST",
         contentType: "application/json;charset=utf-8",
@@ -262,4 +276,49 @@ function clearTextBox() {
     var y = fecha.getFullYear();
     var dateString = (d <= 9 ? '0' + d : d) + '/' + (m <= 9 ? '0' + m : m) + '/' + y;
     $("#FechaIngreso").val(dateString);
+}
+
+function clearModal() {
+    $('$EntryDetailId').val("");
+    $('$CodBarras').val("");
+    $('$NombreProducto').val("");
+    $('$NombreFabricante').val("");
+    $('$RegInvima').val("");
+    $('$Cantidad').val("");
+    $('$FechaVence').val("");
+    $('$Lote').val("");
+}
+
+function buscarProduct() {
+    var CodBarra = $('#CodBarras').val();
+    if (CodBarra.length == 13) {
+        $.ajax({
+            url: "/ProductPresentationPrices/BuscarProducto",
+            data: '{CodBarras: "' + CodBarra + '" }',
+            type: "POST",
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            success: function (result) {
+                $('#ProductDetailId').val(result.ProductDetailId);
+                $('#NombreProducto').val(result.NombreProducto);
+                $('#NombreFabricante').val(result.NombreFabricante);
+                $('#RegInvima').val(result.RegInvima);
+            },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
+            }
+        });
+    }
+    else {
+        swal.fire({
+            title: "Estimado Usuario",
+            text: "El codigo de barras debe tener una longitud de 13 caracteres.",
+            icon: 'info',
+            showCancelButton: false,
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Aceptar",
+        });
+        return false;
+
+    }
 }
